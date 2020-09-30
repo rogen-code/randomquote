ad618af161f1e02bbdc0ac0aa4c96401e5bb1537
Josh Rogen <rogenjh@gmail.com>
2020-09-30 08:33:36 -0400

    added basic server for static file

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use('/', express.static('build'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
